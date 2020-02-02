cc27ef35135c64277789c916fbd4f60c129f30d0
Thibaut PONCHON <thibaut.ponchon@gmail.com>
2020-02-01 19:20:06 -0500

    Update canon and thibaut navmesh

--- GGJ2020/Assets/Scripts/Traps/Canon.cs
+++ GGJ2020/Assets/Scripts/Traps/Canon.cs
@@ -7,13 +7,16 @@ public class Canon : MonoBehaviour
     [SerializeField] private GameObject target = null;
     [SerializeField] private GameObject bullet;
     [SerializeField] private float range = 50f;
-    [SerializeField] private float rotationSpeed = 10.0f;
+    [SerializeField] private float canonSpeed = 10.0f;
     [SerializeField] private Animator canonAnim;
     [SerializeField] private Manager.GameManager gameManager;
+
     private Quaternion m_lookRotation;
+    private Vector3 m_lastKnownPosition = Vector3.zero;
     private bool recovery;
     private float timer;
     private int cooldown;
+    
 
     void Start()
     {
@@ -24,13 +27,20 @@ public class Canon : MonoBehaviour
     {
         if (target != null && Vector3.Distance(transform.position, target.transform.position) <= range)
         {
-            m_lookRotation = Quaternion.LookRotation(target.transform.position - transform.position);
+            if (m_lastKnownPosition != target.transform.position)
+            {
+                m_lastKnownPosition = target.transform.position;
+                m_lookRotation = Quaternion.LookRotation(m_lastKnownPosition - transform.position);
+            }
 
             if (transform.rotation != m_lookRotation)
             {
                 transform.rotation =
-                    Quaternion.RotateTowards(transform.rotation, m_lookRotation, rotationSpeed * Time.deltaTime);
+                    Quaternion.RotateTowards(transform.rotation, m_lookRotation, canonSpeed * Time.deltaTime);
             }
+
+            //float angle = Vector3.Angle(transform.position, target.transform.position);
+            //ransform.RotateAround(transform.position, Vector3.up, angle);
             if (!recovery)
             {
                 Shoot();
